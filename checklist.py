d2abf168b6a8f23cdf9e574b6d41a214cfacc291
Neven <neven@nerk711.(none)>
2014-03-10 18:18:07 +0100

    made some logical changes to permissions in add_permision and show_list

--- checklist.py
+++ checklist.py
@@ -115,33 +115,45 @@ def add_list():
     if request.method == 'POST':
         name = request.form['list_name']
         author = session['username']
+        author_id = session['id']
         
         db = get_db()
         db.execute(
-            'INSERT INTO lists (name, author) VALUES (?, ?);',
-             [name , author]
+            'INSERT INTO lists (name, author_id, author) VALUES (?, ?, ?);',
+             [name, author_id, author]
              )
         db.commit()
         session["permissions"] = get_permissions(session["id"], session["username"]) 
         flash(str(name) + " list added")
-        redirect(url_for('home'))
+        return redirect(url_for('home'))
     return render_template('add_list.html')
 
 
 @app.route('/<int:list_id>/')
 @login_required
 @permissions_required
-def show_list(list_id):   
-    query = """
-    SELECT p.body, l.author, p.comment, p.status, p.id, p.post_time
+def show_list(list_id):
+    author_query = """
+    SELECT author
+    FROM lists
+    WHERE id = ?;
+    """   
+    post_query = """
+    SELECT l.author as list_author, p.body, p.comment, p.status, p.id, p.post_time, p.author
     FROM lists l, posts p
     WHERE p.list_id = l.id
     AND l.id = ?
     ORDER BY p.post_time DESC;
-    """
-    
-    posts = query_db(query, [list_id])    
-    return render_template('list_view.html', posts=posts, list_id=list_id)
+    """    
+    posts = query_db(post_query, [list_id])
+
+    if not posts:
+        #first index must be the author because it's the only query result
+        author = query_db(author_query, [list_id], one=True)[0]
+    else:
+        #extracts list author, no need for another query
+        author = posts[0]["list_author"]        
+    return render_template('list_view.html', posts=posts, list_id=list_id, author=author)
 
 @app.route('/<int:list_id>/add_permission', methods=['GET', 'POST'])
 @login_required
@@ -180,49 +192,23 @@ def add_permission(list_id):
 @permissions_required
 def add_post(list_id):
     if request.method == 'POST':
-        db = get_db()
-
+        
         body = request.form['body']
         comment = request.form['comment']
-        status = request.form['status']        
+        status = request.form['status'] 
+        author = session["username"]       
         post_time = get_current_time()
 
+        db = get_db()
         db.execute(
-            'INSERT INTO posts (list_id, body, comment, status, post_time) VALUES (?, ?, ?, ?, ?);',
-             [list_id ,body, comment, status, post_time]
+            'INSERT INTO posts (list_id, body, comment, status, post_time, author) VALUES (?, ?, ?, ?, ?, ?);',
+             [list_id, body, comment, status, post_time, author]
              )
         db.commit()
         #TODO FLASH MESSAGE HERE        
         return redirect("/{0}/".format(list_id))
 
-    return render_template('add_post.html')
-
-#LEGACY?
-#ajax calls this to fill out new post fragments
-@app.route('/<int:post_num>/show_post')
-@login_required
-@permissions_required
-def show_post(post_num):
-    post = query_db('SELECT * FROM POSTS WHERE ID=?', [post_num], one=True)
-
-    return render_template('post_fragment.html', post=post)
-
-#add editing ability
-#LEGACY
-# @app.route('/<int:list_id>/edit/', methods=['GET', 'POST'])
-# @login_required
-# def edit_post(post_num):    
-#     if request.method == 'POST':
-#         text = request.form['text']
-
-#         db = get_db()
-#         db.execute('UPDATE posts SET body="{0}" WHERE ID={1}'.format(text, post_num))
-#         db.commit()
-#         #return redirect(url_for('home'))
-
-#     text = query_db('SELECT body FROM posts WHERE ID=%s' % post_num, one=True)    
-
-#     return render_template('edit.html', text=text, post_num=post_num)
+    return render_template('add_post.html', list_id=list_id)
 
 #jeditable uses this edit function
 @app.route('/<int:list_id>/edit', methods=['POST'])
@@ -277,6 +263,16 @@ def change_status(list_id):
 def register():
     if request.method == 'POST':
         username = request.form['username']
+        password = request.form['password']
+        duplicate_pass = request.form['duplicate_pass']
+
+        if find_username(username):
+            flash("username already exists!")
+            return render_template("register.html")
+        if not password == duplicate_pass:
+            flash("passwords didn't match")
+            return render_template("register.html")
+
         password = bcrypt.generate_password_hash(request.form['password'])         
         join_date = get_current_time()
 
@@ -295,7 +291,7 @@ def login():
     if request.method == 'POST':
         logged_in = False
 
-        username = request.form['username']
+        username = request.form['username']        
         password = request.form['password']
         user = query_db(
             'SELECT username, password, id FROM users WHERE username=?;', [username], 
